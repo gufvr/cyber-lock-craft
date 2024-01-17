da3c73b47d2ce4ed57c1b042459a3e3714785054
gustavo <gustavo.vfavero@gmail.com>
2024-01-16 21:39:41 -0300

    feat: Import random and string libraries and define machine choices

---
+++ cyber_lock_craft.py
@@ -0,0 +1,7 @@
+import random
+import string
+
+def password_generator(Len_pass = 8):
+    ascii_options = string.ascii_letters
+    number_options = string.digits
+    punt_options = string.punctuation
