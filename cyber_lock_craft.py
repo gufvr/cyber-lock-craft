9c87a82246b1f36aba33ec9376bebc3e90c96f47
gustavo <gustavo.vfavero@gmail.com>
2024-01-16 21:51:39 -0300

    refactor: Consolidate variables for comprehensive grouping and implement loop for random outcomes

--- cyber_lock_craft.py
+++ cyber_lock_craft.py
@@ -5,3 +5,10 @@ def password_generator(Len_pass = 8):
     ascii_options = string.ascii_letters
     number_options = string.digits
     punt_options = string.punctuation
+    options = ascii_options + number_options + punt_options
+
+    password_user = "",
+
+    for i in range(0, Len_pass):
+        digit = random.choice(options)
+        password_user = password_user + digit
