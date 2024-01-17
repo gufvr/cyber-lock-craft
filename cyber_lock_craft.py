af6e47ba04b0a6422f24d5703aaf29990de726c5
gustavo <gustavo.vfavero@gmail.com>
2024-01-16 22:21:09 -0300

    chore: Project completion

--- cyber_lock_craft.py
+++ cyber_lock_craft.py
@@ -7,8 +7,7 @@ def password_generator(Len_pass = 8):
     punt_options = string.punctuation
     options = ascii_options + number_options + punt_options
 
-    password_user = "",
-
+    password_user = ""
     for i in range(0, Len_pass):
         digit = random.choice(options)
         password_user = password_user + digit
